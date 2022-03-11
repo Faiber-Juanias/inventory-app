094b01c26702f9fb75dffe6eed8b20dc128e16f2
Faiber Juanias <faiberjuanias001@gmail.com>
2022-03-10 21:40:23 -0500

    Se crea clase de utileria para guardar y actualizar un producto

--- src/app/form-util/form-send.ts
+++ src/app/form-util/form-send.ts
@@ -1,3 +1,4 @@
+import { DatePipe } from "@angular/common";
 import { FormGroup } from "@angular/forms";
 import { Product } from "../models/product";
 
@@ -5,21 +6,15 @@ export class FormUtil {
 
     static castFormSaveToProduct(form: FormGroup) {
 
-        let id: number = 0;
         let nombre: string = '';
         let cantidad: number = 0;
         let fechaIngreso: string = '';
         let usuarioRegistro: number = 0;
-        let usuarioActualiza: number = 0;
-        let fechaActualiza: string = '';
 
-        if (form.get('idProduct')?.value != null) { id = form.get('idProduct')?.value; }
         if (form.get('nombre')?.value != null) { nombre = form.get('nombre')?.value; }
         if (form.get('cantidad')?.value != null) { cantidad = form.get('cantidad')?.value; }
         if (form.get('fechaIngreso')?.value != null) { fechaIngreso = form.get('fechaIngreso')?.value; }
         if (form.get('usuarioRegistro')?.value != null) { usuarioRegistro = form.get('usuarioRegistro')?.value; }
-        if (form.get('usuarioActualiza')?.value != null) { usuarioActualiza = form.get('usuarioActualiza')?.value; }
-        if (form.get('fechaActualiza')?.value != null) { fechaActualiza = form.get('fechaActualiza')?.value; }
 
         return <Product> {
             "nombreProducto": nombre,
@@ -28,6 +23,38 @@ export class FormUtil {
             "usuarioRegistro": {
                 "idUsuario": usuarioRegistro
             }
+        }
+
+    }
+
+    static castFormUpdateToProduct(form: FormGroup, datepipe: DatePipe) {
+
+        let id: number = 0;
+        let nombre: string = '';
+        let cantidad: number = 0;
+        let fechaIngreso: string = '';
+        let usuarioRegistro: number = 0;
+        let usuarioActualiza: number = 0;
+
+        if (form.get('idProduct')?.value != null) { id = form.get('idProduct')?.value; }
+        if (form.get('nombre')?.value != null) { nombre = form.get('nombre')?.value; }
+        if (form.get('cantidad')?.value != null) { cantidad = form.get('cantidad')?.value; }
+        if (form.get('fechaIngreso')?.value != null) { fechaIngreso = form.get('fechaIngreso')?.value; }
+        if (form.get('usuarioRegistro')?.value != null) { usuarioRegistro = form.get('usuarioRegistro')?.value; }
+        if (form.get('usuarioActualiza')?.value != null) { usuarioActualiza = form.get('usuarioActualiza')?.value; }
+
+        return <Product> {
+            "idProducto": id,
+            "nombreProducto": nombre,
+            "cantidad": cantidad,
+            "fechaIngreso": fechaIngreso,
+            "usuarioRegistro": {
+                "idUsuario": usuarioRegistro
+            },
+            "usuarioActualiza": {
+                "idUsuario": usuarioActualiza
+            },
+            "fechaActualiza": datepipe.transform(new Date(), 'yyyy-MM-ddTHH:mm:ss')
         }
 
     }
